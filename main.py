5498da717ee44b882690b1d3af80c310772794b2
jmlocek <jmlocek@rbcpolska.pl>
2025-11-18 14:59:27 +0100

    single agent changes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from evaluations_with_chain import evaluate_vulnerability_fix_dataset
+from evaluations_with_singleagent import evaluate_vulnerability_fix_dataset
 
 if __name__ == "__main__":
     evaluate_vulnerability_fix_dataset()
